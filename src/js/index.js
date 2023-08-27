7e3383d25c92d2de80557468c1077d2ab2269aba
Ondřej Vitula <vitula@tectronik.cz>
2023-08-27 21:58:13 +0200

    Update URL

--- src/js/index.js
+++ src/js/index.js
@@ -99,7 +99,7 @@ if(form) {
             const [finalLng, finalLat] = fcns.calculateCoordTransform(circleRadio, formEls, coords);
             
             // Constructing URL and redirecting the user to the 'found' page
-            const url = new URL(`/found.html?lng=${finalLng}&lat=${finalLat}`, document.location.href);
+            const url = new URL(`/randomvylet/found.html?lng=${finalLng}&lat=${finalLat}`, document.location.href);
             document.location.href = url;
 
         } else {
